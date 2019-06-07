99e6e8d35c045ed899a72a8cf24ccdea445b1876
chloeosgood <46794566+chloeosgood@users.noreply.github.com>
2019-06-07 16:43:10 -0500

    Update README and clean up code
    Added test cases for each file type it shoud be able to read.

--- debug/gulpfile.ts
+++ debug/gulpfile.ts
@@ -1,34 +1,20 @@
 let gulp = require('gulp')
-import { tapCsv } from '../src/plugin'
-import { tapXlsx } from '../src/plugin-tap-xlsx'
+import { tapSpreadSheet } from '../src/plugin-tap-xlsx'
+import * as rename from 'gulp-rename'
+import Vinyl = require('vinyl') 
+const pkginfo = require('pkginfo')(module); // project package.json info into module.exports
+const PLUGIN_NAME = module.exports.name;
+const errorHandler = require('gulp-error-handle'); // handle all errors in one handler, but still stop the stream if there are errors
 import * as loglevel from 'loglevel'
 const log = loglevel.getLogger('gulpfile')
 log.setLevel((process.env.DEBUG_LEVEL || 'warn') as log.LogLevelDesc)
-import XLSX =require('xlsx');
 // if needed, you can control the plugin's logging level separately from 'gulpfile' logging above
 // const pluginLog = loglevel.getLogger(PLUGIN_NAME)
 // pluginLog.setLevel('debug')
 
-import * as rename from 'gulp-rename'
-const errorHandler = require('gulp-error-handle'); // handle all errors in one handler, but still stop the stream if there are errors
-
-const pkginfo = require('pkginfo')(module); // project package.json info into module.exports
-const PLUGIN_NAME = module.exports.name;
-
-import Vinyl = require('vinyl') 
-
-let gulpBufferMode = false;
-
-function switchToBuffer(callback: any) {
-  gulpBufferMode = true;
-
-  callback();
-}
-
-function runTapCsv(callback: any) {
+function runTapSpreadSheet(callback: any) {
   log.info('gulp task starting for ' + PLUGIN_NAME)
-  return gulp.src('../freshmen classes.xlsx')
-  // return gulp.src('../testdata/*.csv',{buffer:gulpBufferMode})
+  return gulp.src(['../testdata/*', '!../testdata/ignore', '!../testdata/processed'])
     .pipe(errorHandler(function(err:any) {
       log.error('Error: ' + err)
       callback(err)
@@ -36,8 +22,8 @@ function runTapCsv(callback: any) {
     .on('data', function (file:Vinyl) {
       log.info('Starting processing on ' + file.basename)
     })    
-    // .pipe(tapCsv({columns:true/*, info:true */}))
-    .pipe(tapXlsx({columns:true/*, info:true */}))
+    .pipe(tapSpreadSheet({columns:true/*, info:true */}))
+    .pipe(rename({ extname: ".ndjson" }))
     .pipe(gulp.dest('../testdata/processed'))
     .on('data', function (file:Vinyl) {
       log.info('Finished processing on ' + file.basename)
@@ -46,38 +32,6 @@ function runTapCsv(callback: any) {
       log.info('gulp task complete')
       callback()
     })
-
 }
 
-export function csvParseWithoutGulp(callback: any) {
-
-  const parse = require('csv-parse')
-
-  var parser = parse({delimiter: ',', columns:true});
-  
-  require('fs').createReadStream('../testdata/cars.csv').pipe(parser)
-  .on("data",(data:any)=>{
-    console.log(data)
-  });
-  
-}
-
-exports.default = gulp.series(runTapCsv)
-exports.runTapCsvBuffer = gulp.series(switchToBuffer, runTapCsv)
-
-//ADD TEST FILES / TAKE OUT CSV FILES
-//CHANGE READ ME
-//CHANGE DEBUG RUNNING ORDER/ GET RID OF SOME 
-//OPTIONS ARE GONNA BE COMPLETELY DIFFERENT FOR EACH PROJECT
-//INCLUDE BUNCH OF TEST FILES!!
-//HAVE TO INCLUDE MULTIPLE DIFFERENT TYPES OF FILES
-//COULD GIVE CHOICES ON WHICH SHEET TO LOOK AT, OR COULD GIVE NO OPTIONS
-//IF THEY PASS IN WORKSHEET NAME, HAVE IT WHERE IT WILL TAKE IT, AND PASS IT INTO THE SHEETNAMES PART
-//IF TIME
-        //IF WE CAN SPILT EACH TAB INTO OWN FILE THAT WOULD BE IDEAL
-
-// XLSX, XLS, DBF, CSV, HTML, ODS
-//INCLUDE ABOVE TEST TYPES, AND TRY TO GET IT TO WHERE ITLL HANDLE THEM
-
-//DATA.GOV OPEN DATA
-//BASICALLY GET THESE TYPES OF TEST FILES
+exports.default = gulp.series(runTapSpreadSheet)
--- src/plugin-tap-xlsx.ts
+++ src/plugin-tap-xlsx.ts
@@ -1,7 +1,7 @@
 const through2 = require('through2');
 import PluginError= require('plugin-error')
 import XLSX = require('xlsx')
-const PLUGIN_NAME= 'gulp-etl-tap-xlsx'
+const PLUGIN_NAME= 'gulp-etl-tap-spreadsheet'
 import * as loglevel from 'loglevel'
 const log = loglevel.getLogger(PLUGIN_NAME)
 
@@ -9,15 +9,14 @@ function createRecord(recordObject:Object, streamName: string) : any {
     return {type:"RECORD", stream:streamName, record:recordObject}
 }
 
+//still gotta get html and dbf
+
 function convertToJson(file:any){
     let workbook = XLSX.read(file.contents, {type:"buffer"}) 
     return XLSX.utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[0]])
-    //RISK IS THAT IT DOESNT WORK ON DIFFERENT TYPES AND JUST QUITS
-    //INCLUDE BUNCH OF TEST CASES
 }
 
-
-export function tapXlsx(configObj:any){
+export function tapSpreadSheet(configObj:any){
     if (!configObj) configObj = {}
     if (!configObj.columns) configObj.columns = true
 
@@ -31,15 +30,14 @@ export function tapXlsx(configObj:any){
             throw new PluginError(PLUGIN_NAME, 'Trying to stream from unstreamable file')
         }
         else if ( file.isBuffer() ){
-            let workbook = XLSX.read(file.contents, {type:"buffer"})
-            let sheetArr = XLSX.utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[0]])
+            let sheetArr = convertToJson(file)
             
             let tempLine: any;
             let resultArray= [];
             for (let dataIdx in sheetArr) {
                 try{
-                    let lineObj = sheetArr[dataIdx]
-                    tempLine = createRecord(lineObj, 'tap-xlsx')
+                    let lineObj:any = sheetArr[dataIdx]
+                    tempLine = createRecord(lineObj, 'tap-spreadsheet')
                     if (tempLine) {
                         let tempStr = JSON.stringify(tempLine)
                         log.debug(tempStr)
@@ -51,7 +49,8 @@ export function tapXlsx(configObj:any){
                 }
             }
             let data: string = resultArray.join('\n')
-            file.contents = Buffer.from(data); 
+            file.contents = Buffer.from(data);
+            // file.rename({extname:".ndjson"}) 
             log.debug('calling callback');
             callback(returnErr, file)
         }
